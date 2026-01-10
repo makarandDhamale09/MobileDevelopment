f7dddeb374ac2fd160c625a43d37a97f42b0323a
Makarand Dhamale <makarand.dhamale09@gmail.com>
2026-01-10 17:15:33 +0530

    Safe area view

--- ShelfieApp/app/(dashboard)/books.jsx
+++ ShelfieApp/app/(dashboard)/books.jsx
@@ -6,7 +6,7 @@ import ThemedText from "../../components/ThemedText";
 
 const Books = () => {
   return (
-    <ThemedView style={styles.container}>
+    <ThemedView style={styles.container} safe={true}>
       <Spacer />
       <ThemedText title={true} style={styles.heading}>
         Your Reading List
--- ShelfieApp/components/ThemedView.jsx
+++ ShelfieApp/components/ThemedView.jsx
@@ -1,12 +1,31 @@
 import { View, useColorScheme } from "react-native";
 import { Colors } from "../constants/Colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
+  const insets = useSafeAreaInsets();
+
+  if (!safe)
+    return (
+      <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    );
 
   return (
-    <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    <View
+      style={[
+        {
+          backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+          paddingLeft: insets.left,
+          paddingRight: insets.right,
+        },
+        style,
+      ]}
+      {...props}
+    />
   );
 };
 

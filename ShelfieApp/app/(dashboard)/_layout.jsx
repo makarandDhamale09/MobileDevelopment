d955665a533b16584633754c431e55159316816d
Makarand Dhamale <makarand.dhamale09@gmail.com>
2026-01-10 16:56:21 +0530

    Add tab icons

--- ShelfieApp/app/(dashboard)/_layout.jsx
+++ ShelfieApp/app/(dashboard)/_layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Stack, Tabs } from "expo-router";
 import { useColorScheme } from "react-native";
 import { Colors } from "../../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function DashboardLayout() {
   const colorScheme = useColorScheme();
@@ -24,18 +25,39 @@ export default function DashboardLayout() {
         name="profile"
         options={{
           tabBarLabel: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={24}
+              color={focused ? theme.iconColorFocused : theme.iconColor}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="books"
         options={{
           tabBarLabel: "Books",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "book" : "book-outline"}
+              size={24}
+              color={focused ? theme.iconColorFocused : theme.iconColor}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="create"
         options={{
           tabBarLabel: "Add Book",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "add-circle" : "add-circle-outline"}
+              size={24}
+              color={focused ? theme.iconColorFocused : theme.iconColor}
+            />
+          ),
         }}
       />
     </Tabs>
